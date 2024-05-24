cab80533d030b247a6a6cc8c2f5f6ee3af6375a0
haldomae <hal.domae@gmail.com>
2024-05-24 17:12:53 +0900

    画像切り替え処理を追加

--- app/src/main/java/com/pi12a083_domaekazuki/kadai03/MainActivity.kt
+++ app/src/main/java/com/pi12a083_domaekazuki/kadai03/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.pi12a083_domaekazuki.kadai03
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // 画像の配列
+    // listは読み取り専用
+    // Arrayは書き込み可能
+    private val imageList = listOf(
+        R.drawable.dog,
+        R.drawable.horse,
+        R.drawable.kitten
+    )
+
+    private val textList = listOf(
+        "aaa",
+        "bbb",
+        "ccc"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +35,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnLeft: ImageButton = findViewById(R.id.btnLef)
+        btnLeft.setOnClickListener{
+            movePosition(0)
+        }
+
+    }
+    private fun movePosition(num: Int){
+        val textView: TextView = findViewById(R.id.textView)
+        val imageView: ImageView = findViewById(R.id.imageView)
+
+        textView.text = textList[0]
+        imageView.setImageResource(imageList[0])
     }
 }
